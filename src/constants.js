b82b3bafb2c6065811d14add643088afc6fbc601
Maxime LAPLANCHE <laplanche.maxime@outlook.fr>
2020-11-30 23:23:40 +0100

    add constants support with .env file

--- src/constants.js
+++ src/constants.js
@@ -1,12 +1,26 @@
+const common = {
+    SITE_NAME: process.env.REACT_APP_SITE_NAME,
+    SITE_DESCRIPTION: process.env.REACT_APP_SITE_DESCRIPTION,
+    SITE_LOGO_URL: process.env.REACT_APP_SITE_LOGO_URL,
+
+    MAIL_CONTACT: process.env.REACT_APP_MAIL_CONTACT,
+        
+    USER_CONTACT: process.env.REACT_APP_USER_CONTACT,
+}
+
 const prod = {
+    common: common,
     url: {
-        API_URL: 'https://dolibarr.mltech.fr/api/index.php',
+        DOLIBARR_INSTANCE: process.env.REACT_APP_DOLIBARR_PROD_INSTANCE,
+        API_URL: process.env.REACT_APP_API_PROD_ADDRESS,
     }
 };
 
 const dev = {
+    common: common,
     url: {
-        API_URL: 'http://localhost:8083/api/index.php',
+        DOLIBARR_INSTANCE: process.env.REACT_APP_DOLIBARR_DEV_INSTANCE,
+        API_URL: process.env.REACT_APP_API_DEV_ADDRESS,
     }
 };
 
